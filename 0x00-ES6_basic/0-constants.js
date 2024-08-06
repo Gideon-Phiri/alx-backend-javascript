798c77c15b218a296fe2ddf9def9ba91bb0a852e
Meltah <gideonphiri032@gmail.com>
2024-08-06 03:34:07 +0200

    Refactor code to use const and let declarations in 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,12 @@
-// taskFirst function using const
 export function taskFirst() {
   const task = "I prefer const when I can.";
   return task;
 }
 
-// getLast function remains the same
 export function getLast() {
   return " is okay";
 }
 
-// taskNext function using let
 export function taskNext() {
   let combination = "But sometimes let";
   combination += getLast();
