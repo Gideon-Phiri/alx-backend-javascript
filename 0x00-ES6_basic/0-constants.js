d7a6374e0341491695d8c33e37762281c8401311
Meltah <gideonphiri032@gmail.com>
2024-08-06 03:13:19 +0200

    feat: Add const and let declarations to 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,5 @@
 export function taskFirst() {
+  // Uses const
   const task = "I prefer const when I can.";
   return task;
 }
@@ -8,9 +9,9 @@ export function getLast() {
 }
 
 export function taskNext() {
+  // Uses let
   let combination = "But sometimes let";
   combination += getLast();
 
   return combination;
-  //
 }
